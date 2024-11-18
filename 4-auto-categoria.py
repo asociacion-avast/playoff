aa452cb453108c00c3d87400ef3d9a7f1be0bbd8
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2024-11-18 15:04:42 +0100

    Add script to fix categories by year and A13,A15

---
+++ 4-auto-categoria.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import configparser
+import datetime
+import os
+
+import dateutil.parser
+
+import common
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~/.avast.ini"))
+
+
+token = common.gettoken(
+    user=config["auth"]["RWusername"], password=config["auth"]["RWpassword"]
+)
+
+headers = {"Authorization": f"Bearer {token}"}
+
+# Definiciones
+avast13 = 66
+avast15 = 65
+
+# Leer datos
+socios = common.readjson("socios")
+categorias = common.readjson("categorias")
+today = datetime.date.today()
+
+
+# Locate our member in the list of members
+for socio in socios:
+    if (
+        "estat" in socio
+        and socio["estat"] == "COLESTVAL"
+        and "estatColegiat" in socio
+        and socio["estatColegiat"]["nom"] == "ESTALTA"
+    ):
+        # ID Socio
+        socioid = socio["idColegiat"]
+
+        # Find our born year
+        try:
+            fecha = dateutil.parser.parse(socio["persona"]["dataNaixement"])
+        except Exception:
+            fecha = False
+            print(f"ERROR: Sin fecha nacimiento para socio ID: {socioid}")
+
+        if fecha:
+            year, month, day = fecha.year, fecha.month, fecha.day
+
+            categoriassocio = []
+            for categoria in socio["colegiatHasModalitats"]:
+                idcategoria = int(categoria["idModalitat"])
+                categoriassocio.append(idcategoria)
+
+                try:
+                    myyear = int(categoria["modalitat"]["nom"])
+                except Exception:
+                    myyear = False
+
+                if myyear:
+                    if myyear != year:
+                        print(f"ERROR: AÑO INCORRECTO para socio ID: {socioid}")
+                        common.delcategoria(token, socioid, idcategoria)
+
+            targetcategorias = []
+            for categoria in categorias:
+                nombre = categoria["nom"]
+
+                # Attempt to find categories for a year
+                try:
+                    mycat = int(nombre)
+                except Exception:
+                    mycat = False
+
+                if mycat and mycat == year and year in range(2000, today.year):
+                    # Our member had a match with the born year
+                    targetcategorias.append(int(categoria["idModalitat"]))
+
+            edad = today.year - year - ((today.month, today.day) < (month, day))
+
+            # Add target category for +13/+15
+            if edad in range(13, 15):
+                # AVAST+13
+                targetcategorias.append(avast13)
+
+            elif edad in range(15, 24):
+                # AVAST+15
+                targetcategorias.append(avast15)
+
+            # El socio no debe estar en grupos A+13 o A+15
+            for i in [avast13, avast15]:
+                if i in categoriassocio and i not in targetcategorias:
+                    print(f"ERROR: Borrando categoria {i} del socio {socioid}")
+                    common.delcategoria(token, socioid, i)
+
+            for categoria in targetcategorias:
+                if categoria not in categoriassocio:
+                    print(
+                        "IFF",
+                        socioid,
+                        categoria,
+                        categoriassocio,
+                        categoria in categoriassocio,
+                    )
+                    response = common.addcategoria(token, socioid, categoria)
