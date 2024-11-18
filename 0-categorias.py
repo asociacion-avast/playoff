e12013ae2f10070c90ff5c229b40c4a9a256794a
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2024-11-18 15:03:52 +0100

    Add script to download categories

---
+++ 0-categorias.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+import configparser
+import os
+
+import requests
+
+import common
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~/.avast.ini"))
+
+
+token = common.gettoken()
+
+categoriasurl = f"{common.apiurl}/modalitats"
+data = {"Authorization": f"Bearer {token}"}
+
+print("Obteniendo listado de categorias")
+result = requests.get(
+    categoriasurl, auth=common.BearerAuth(token), headers=common.headers
+)
+
+categorias = result.json()
+
+validids = []
+invalidids = []
+
+print("Saving file to disk")
+
+common.writejson(filename="categorias", data=categorias)
