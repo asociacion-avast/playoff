5bf88263e36f9e72a7272aa64feed63428014746
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2025-03-31 22:54:38 +0200

    Add script for opening and enrolling users

---
+++ 5-abreactividad.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import configparser
+import datetime
+import os
+import pprint
+import sys
+
+import common
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~/.avast.ini"))
+
+
+token = common.gettoken(
+    user=config["auth"]["RWusername"], password=config["auth"]["RWpassword"]
+)
+
+
+data = {"Authorization": f"Bearer {token}"}
+
+
+idActivitat = False
+if len(sys.argv) > 1:
+    try:
+        idActivitat = int(sys.argv[1])
+    except Exception:
+        idActivitat = False
+
+if not idActivitat:
+    print("Actividad no indicada")
+    sys.exit(-1)
+
+
+null = ""
+false = False
+true = True
+
+
+# Get today date
+today = datetime.datetime.now()
+tomorrow = today + datetime.timedelta(days=1)
+
+override = {
+    "dataInici": "%s-%s-%s %s:%s"
+    % (today.year, today.month, today.day, today.hour, today.minute),
+    "dataLimit": "%s-%s-%s %s:%s"
+    % (tomorrow.year, tomorrow.month, tomorrow.day, tomorrow.hour, tomorrow.minute),
+}
+
+print("Haciendo llamada API")
+pprint.pprint(common.editaactividad(token, idActivitat, override))
---
+++ 5-inscribeactividad.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import configparser
+import os
+import sys
+
+import common
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser("~/.avast.ini"))
+
+
+token = common.gettoken(
+    user=config["auth"]["RWusername"], password=config["auth"]["RWpassword"]
+)
+
+
+idActivitat = False
+idColegiat = False
+if len(sys.argv) > 2:
+    try:
+        idActivitat = int(sys.argv[1])
+    except Exception:
+        idActivitat = False
+    try:
+        idColegiat = int(sys.argv[2])
+    except Exception:
+        idColegiat = False
+
+
+if not idActivitat:
+    print("Actividad no indicada")
+    print("inscribeactividad.py idActivitat idColegiat")
+    sys.exit(-1)
+
+if not idColegiat:
+    print("Colegiat no indicado")
+    print("inscribeactividad.py idActivitat idColegiat")
+    sys.exit(-1)
+
+
+print("Procesando actividad: %s" % idActivitat)
+
+print(
+    common.create_inscripcio(
+        token=token, idActivitat=idActivitat, idColegiat=idColegiat
+    ).text
+)
